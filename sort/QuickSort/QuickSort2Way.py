ca6b17fb25f773af6b3d0778f1ce46782fad5ec4
王亚晨 <floatnight@wangyachendeMacBook-Pro.local>
2018-10-20 21:39:48 +0800

    双路快排

--- sort/QuickSort/QuickSort2Way.py
+++ sort/QuickSort/QuickSort2Way.py
@@ -1,17 +1,20 @@
-#双路随机快速排序
+#双路快排
 from random import randint
-def QuickSort2(arr):
+def QuickSort2Way(arr):
     
     def partition(arr,l,r):
-        if l+1 >= r:return
-        a,b=randint(l,r-1),l
-        for c in range(l,r):
-            if c == a:continue
-            if arr[a] >arr[c]:
-                b+=1
-                arr[b],arr[c] = arr[c],arr[b]
-        else:arr[a],arr[b] = arr[b],arr[a]
-        partition(arr,l,b)
-        partition(arr,b+1,r)
-    partition(arr,0,len(arr)) 
-    
+        if l >= r:return
+        index,i,j = randint(l,r),l+1,r
+        arr[index],arr[l] = arr[l],arr[index]
+        while True:
+            while arr[i]<=arr[l] and i<r: i+=1
+            while arr[j]>=arr[l] and j>l: j-=1
+            if i>=j:
+                arr[l],arr[j] = arr[j],arr[l]
+                break
+            else:arr[i],arr[j] = arr[j],arr[i]
+            i+=1
+            j-=1
+        partition( arr,l,j-1 )
+        partition( arr,j+1,r )
+    partition( arr,0,len(arr)-1 )    
