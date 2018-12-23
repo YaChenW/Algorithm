94e9f6c19e8c8f8c712cafc3e4fd41d19e43b7fd
FloatKnight <38676076+FloatKnight@users.noreply.github.com>
2018-12-23 13:19:48 +0800

    Update Selection Sort.py

--- sort/Selection Sort.py
+++ sort/Selection Sort.py
@@ -1,15 +1,11 @@
 # 选择排序
 def selectionSort(arr):
-    if len(arr) < 1:
-        return arr
-
+    if len(arr) < 1: return arr
     n = len(arr)
     for i in range(n):
         minIndex = i
         for j in range(i + 1, n):
             if arr[j] < arr[minIndex]:
                 minIndex = j
-        change = arr[i]
-        arr[i] = arr[minIndex]
-        arr[minIndex] = change
+        arr[i],arr[minIndex] = arr[minIndex],arr[i]
     return arr
