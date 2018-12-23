dc9227f2916550750a915b3b22998ec5c228276f
FloatKnight <38676076+FloatKnight@users.noreply.github.com>
2018-12-23 15:02:20 +0800

    Update Insert Sort.py

--- sort/Insert Sort.py
+++ sort/Insert Sort.py
@@ -1,16 +1,10 @@
 # 插入排序,数组的越有序,速度越块,对完全有序数组排序,时间复杂度为O(n)
 def insertSort(arr):
-    if len(arr) < 1:
-        return arr
+    if len(arr) <= 1:return arr
 
-    for i in range(1, len(arr)):
-        minNum = arr[i]
-        for j in range(i, 0, -1):
-            if minNum < arr[j - 1]:
-                arr[j] = arr[j - 1]
-            else:
-                arr[j] = minNum
+    for i in range(0, len(arr)-1):
+        for j in range(i, -1, -1):
+            if arr[j] < arr[j + 1]:
                 break
-        else:
-            arr[j - 1] = minNum
+            arr[j],arr[j+1] = arr[j+1],arr[j]
     return arr
